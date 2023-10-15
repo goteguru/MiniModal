3dfdb868681497ac4d42f3005764a45cca5f536c
Mészáros Gergely <goteguru@gmail.com>
2023-10-15 21:39:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# MiniModal
-Minimal javascript async modal functionality for spinners, forms, warnings
+# Mini Modal
+
+Minimal async modal functionality for simple apps.
+You can await for a form input or
+
+## minimal API
+
+* create modal windows from html templates
+* open modal and await for it in async functions or modules
+* alternatively, listen to `submitModal` or `cancelModal` messages
+* generate async Exception on cancel event (or just ignore it)
+
+The blocker div class called `.modal-background` if you want to style it.
+
+```
+.modal-background { background-color: rgb(10,10,10,0.6) }
+```
+
+## Usage:
+
+import in modules:
+```
+import { Modal } from './modal.js'
+```
+
+Create modal content from template and call it multiple times:
+```
+const modalForm = new Modal('#form-template')
+... do things ...
+const input = await modalForm.open()
+... process input ...
+const input2 = await modalForm.open()
+```
+
+Template can contain style :
+```
+<template id="form-template">
+	<form class="modal-window">
+		<div class="modal-submit" name="esc">×</div>
+		<label for="question">Question:</label>
+		<input type="input" id="question" name="Question" value="some-default">
+		<div class="modal-buttonrow">
+			<button type="submit" name="button" value="1">yes</button>
+		</div>
+	</form>
+	<style>
+		.modal-submit { margin:0; display: block; width: 20px; height: 20px; position:absolute; top:0; right:0; cursor:pointer;}
+		.modal-buttonrow { bottom:0; left:0; width:100%}
+	</style>
+</template>
+```
---
+++ modal.js
@@ -0,0 +1,82 @@
+export function Modal(tpl, { exception=false, interactive=true } = {}) {
+	const template = document.querySelector(tpl)
+	if (!template) throw new Error(`Missing template: ${tpl}`)
+	const clone = template.content.cloneNode(true)
+
+	;[...clone.querySelectorAll(".modal-cancel")].forEach(el => {
+		el.addEventListener('click', _ => this.cancel())
+		el.style.cursor="pointer"
+	})
+
+	;[...clone.querySelectorAll(".modal-submit")].forEach(el => {
+		el.addEventListener('click', _ => this.submit())
+	})
+
+	;[...clone.querySelectorAll("form")].forEach(el => {
+		el.addEventListener('submit', e => {
+			e.preventDefault()
+			if (e.submitter && e.submitter.name)
+				this.submit({[e.submitter.name]: e.submitter.value})
+			else
+				this.submit()
+		})
+	})
+
+	this.exception = exception ?? true
+	this.root = document.createElement('div')
+	this.root.style.width = '100%'
+	this.root.style.height = '100%'
+	this.root.style.position = 'fixed'
+	this.root.style.top = '0'
+	this.root.style.left = '0'
+	this.root.style['z-index'] = '200'
+	this.root.style.display = 'flex'
+	this.root.style['justify-content'] = 'space-evenly'
+	this.root.style['flex-direction'] = 'column'
+	this.root.tabIndex = '0' // needs for key focus
+	this.root.classList.add('modal-background')
+	this.root.appendChild(clone)
+
+	this.root.addEventListener("cancelModal", _ => {
+		this.root.parentNode?.removeChild(this.root)
+		this.exception
+			? this.onCancel(new Error("Cancelled"))
+			: this.onSubmit({})
+	})
+
+	this.root.addEventListener("submitModal", event => {
+		this.root.parentNode?.removeChild(this.root)
+		this.onSubmit(event.detail)
+	})
+
+	if (interactive) this.root.onkeyup = e => {
+		if (e.key == "Enter") this.submit()
+		if (e.key == "Escape") this.cancel()
+	}
+
+	this.onCancel = _=>_
+	this.onSubmit = _=>_
+}
+
+Modal.prototype.cancel = function() {
+	this.root.dispatchEvent(new Event("cancelModal"))
+}
+
+Modal.prototype.submit = function(override={}) {
+	const inputs = this.root.querySelectorAll("input:not([type=radio]:not(:checked)),meter,progress,select")
+	const inpobj = Object.fromEntries([...inputs].map(i=>[i.name, i.value]))
+	const detail = {...inpobj, ...override}
+	this.root.dispatchEvent(new CustomEvent("submitModal", {detail}))
+}
+
+Modal.prototype.open = function() {
+	document.body.appendChild(this.root)
+	this.root.focus()
+	return new Promise((res,rej) => {
+		this.onSubmit = res
+		this.onCancel = rej
+	})
+}
+
+// Factory functions
+export const createModal = tpl => new Modal(tpl)
